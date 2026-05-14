d7f6b093248f506dbde32894abe717f04ea86b5f
Abhishek-Puri-Goswami <ap.goswami.2854@gmail.com>
2026-05-14 14:27:52 +0530

    [Abhishek Puri Goswami] : chore - clean up old documentation files and update database init script

--- init-databases.sql
+++ init-databases.sql
@@ -7,14 +7,14 @@ CREATE DATABASE IF NOT EXISTS connecthub_notification;
 CREATE DATABASE IF NOT EXISTS connecthub_payment;
 
 -- Create application user that services connect as
-CREATE USER IF NOT EXISTS 'parents'@'%' IDENTIFIED BY 'parents@top';
+CREATE USER IF NOT EXISTS 'dbadmin'@'%' IDENTIFIED BY 'Anaya^123456';
 
 -- Grant privileges on all service databases
-GRANT ALL PRIVILEGES ON connecthub_auth.*         TO 'parents'@'%';
-GRANT ALL PRIVILEGES ON connecthub_room.*         TO 'parents'@'%';
-GRANT ALL PRIVILEGES ON connecthub_message.*      TO 'parents'@'%';
-GRANT ALL PRIVILEGES ON connecthub_media.*        TO 'parents'@'%';
-GRANT ALL PRIVILEGES ON connecthub_notification.* TO 'parents'@'%';
-GRANT ALL PRIVILEGES ON connecthub_payment.*      TO 'parents'@'%';
+GRANT ALL PRIVILEGES ON connecthub_auth.*         TO 'dbadmin'@'%';
+GRANT ALL PRIVILEGES ON connecthub_room.*         TO 'dbadmin'@'%';
+GRANT ALL PRIVILEGES ON connecthub_message.*      TO 'dbadmin'@'%';
+GRANT ALL PRIVILEGES ON connecthub_media.*        TO 'dbadmin'@'%';
+GRANT ALL PRIVILEGES ON connecthub_notification.* TO 'dbadmin'@'%';
+GRANT ALL PRIVILEGES ON connecthub_payment.*      TO 'dbadmin'@'%';
 
 FLUSH PRIVILEGES;
